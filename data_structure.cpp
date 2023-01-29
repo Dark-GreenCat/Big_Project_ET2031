d18c54ca261352652937b836e5ad681d3a510a12
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2023-01-29 16:57:34 +0700

    Merge manually data_structure.cpp from experimental-huy

--- data_structure.cpp
+++ data_structure.cpp
@@ -1,8 +1,7 @@
 #include "data_structure.h"
 Price default_money;
 
-ShippingForm::ShippingForm()
-{
+ShippingForm::ShippingForm() {
 	this->ID = DEFAULT_ID;
 	this->sender_name = BLANK_TEXT;
 	this->receiver_name = BLANK_TEXT;
@@ -12,6 +11,30 @@ ShippingForm::ShippingForm()
 	this->received_date = NULL_DATE;
 
 	this->isSucceeded = false;
+}
+
+ShippingForm::~ShippingForm() {
+	delete this;
+}
+
+void ShippingForm::inputGeneralInfo() {
+	std::cin.ignore();
+	std::cout << "Sender's name: ";
+	getline(std::cin, sender_name); 
+	std::cout << "From address: ";
+	getline(std::cin, from_address); //std::cin.ignore(MAX_STREAMSIZE, '\n');
+	std::cout << "Sent date (yyyymmdd): ";
+	std::cin >> sent_date;
+
+	std::cout << std::endl;
+
+	std::cin.ignore();
+	std::cout << "Receiver's name: ";
+	getline(std::cin, receiver_name);
+	std::cout << "To address: ";
+	getline(std::cin, to_address);
+	std::cout << "Received date (yyyymmdd): ";
+	std::cin >> received_date;
 }
 
 void ShippingForm::inputGeneralInfo(std::ifstream &filein)
@@ -35,8 +58,7 @@ void ShippingForm::inputGeneralInfo(std::ifstream &filein)
 	filein >> received_date;
 }
 
-void ShippingForm::outputGeneralInfo()
-{
+void ShippingForm::outputGeneralInfo() {
 	std::cout << "Sender's name: " << sender_name;
 	std::cout << "\nFrom address: " << from_address;
 	std::cout << "\nSent date: " << convertDate(sent_date);
@@ -49,9 +71,13 @@ void ShippingForm::outputGeneralInfo()
 }
 
 /////////////////////////////////////
-double DocumentShippingForm::getShippingPrice(Price custom_price)
-{
+double DocumentShippingForm::getShippingPrice(Price custom_price) {
 	return (distance * custom_price.DOC_distance + custom_price.DOC_service);
+}
+
+void DocumentShippingForm::inputDetailInfo() {
+	std::cout << "Enter distance (km): ";
+	std::cin >> distance;
 }
 
 void DocumentShippingForm::inputInfo(std::ifstream &filein)
@@ -60,15 +86,21 @@ void DocumentShippingForm::inputInfo(std::ifstream &filein)
 	filein >> distance;
 }
 
-void DocumentShippingForm::outputInfo()
-{
+void DocumentShippingForm::outputDetailInfo() {
 	std::cout << "Distance (km): " << distance;
 }
 
 /////////////////////////////////////
-double PackageShippingForm::getShippingPrice(Price custom_price)
-{
+double PackageShippingForm::getShippingPrice(Price custom_price){
 	return (distance * custom_price.PAC_distance + weight * custom_price.PAC_weight);
+}
+
+void PackageShippingForm::inputDetailInfo() {
+	std::cout << "Enter distance (km): ";
+	std::cin >> distance;
+
+	std::cout << "Enter weight (kg): ";
+	std::cin >> weight;
 }
 
 void PackageShippingForm::inputInfo(std::ifstream &filein)
@@ -80,28 +112,33 @@ void PackageShippingForm::inputInfo(std::ifstream &filein)
 	filein >> weight;
 }
 
-void PackageShippingForm::outputInfo()
-{
+void PackageShippingForm::outputDetailInfo() {
 	std::cout << "Distance (km): " << distance << std::endl;
 	std::cout << "Weight (kg): " << weight;
 }
 
 ///////////////////////////////////
-void ShippingFormList::addForm(ShippingForm* &Form)
-{
+void ShippingFormList::addForm(ShippingForm* &Form) {
 	this->FormList.push_back(Form);
 }
 
-void ShippingFormList::inputList(std::ifstream &filein)
-{
+void ShippingFormList::removeForm(int index) {
+	this->FormList.erase(FormList.begin() + index);
+}
+
+void ShippingFormList::replaceForm(ShippingForm* &NewForm, int index) {
+	this->FormList.insert(FormList.begin() + index, NewForm);
+	this->removeForm(index + 1);
+}
+
+void ShippingFormList::inputList(std::ifstream &filein) {
 	char choice = 'N';
 	std::cout << "The current database has " << FormList.size() << " forms\n";
 	std::cout << "Do you want to add more forms? (Y/N) :";
 	filein >> choice;
 
 	if (choice == 'Y' || choice == 'y')
-		do
-		{
+		do {
 			ShippingForm* Form;
 			inputForm(Form, filein);
 
@@ -112,17 +149,23 @@ void ShippingFormList::inputList(std::ifstream &filein)
 			filein >> choice;
 		} while (choice == 'Y' || choice == 'y');
 }
-void ShippingFormList::outputList()
-{
-	std::cout << "\nNumber of form: " << FormList.size() << std::endl;
-	for (int i = 0; i < FormList.size(); i++)
-	{
-		std::cout << "\nForm #" << (i + 1) << std::endl;
-		outputAllFormInfo(*FormList.at(i));
-		std::cout << std::endl;
-	}
-}
+
 ////////////////////////////
+void inputForm(ShippingForm* &Form) {
+	int type;
+	std::cout << "Enter type of parcel: " << std::endl;
+	std::cout << "1. Document\t2.Package\n";
+	std::cout << "Type: ";
+	std::cin >> type;
+	if (type == DOCUMENT)
+		Form = new DocumentShippingForm;
+	else if (type == PACKAGE)
+		Form = new PackageShippingForm;
+
+	Form->inputGeneralInfo();
+	Form->inputDetailInfo();
+}
+
 void inputForm(ShippingForm* &Form, std::ifstream &filein)
 {
 	int type;
@@ -139,9 +182,99 @@ void inputForm(ShippingForm* &Form, std::ifstream &filein)
 	Form->inputInfo(filein);
 }
 
-void outputAllFormInfo(ShippingForm &Form)
-{
+void outputAllFormInfo(ShippingForm &Form) {
 	Form.outputGeneralInfo();
 	std::cout << std::endl;
-	Form.outputInfo();
+	Form.outputDetailInfo();
+}
+
+void inputFormList(ShippingFormList &List) {
+	char choice = 'N';
+	std::cout << "The current database has " << List.FormList.size() << " forms\n";
+	std::cout << "Do you want to add more forms? (Y/N) :";
+	std::cin >> choice;
+
+	if (choice == 'Y' || choice == 'y')
+		do {
+			ShippingForm *Form;
+			inputForm(Form);
+
+			List.FormList.push_back(Form);
+
+			std::cout << "\nThe current database has " << List.FormList.size() << " forms\n";
+			std::cout << "Do you want to add more forms? (Y/N) :";
+			std::cin >> choice;
+		} while (choice == 'Y' || choice == 'y');
+}
+
+void printFormList(ShippingFormList &List) {
+	std::cout << "\nNumber of form: " << List.FormList.size() << std::endl;
+	for(int i = 0; i < List.FormList.size(); i++) {
+		std::cout << "\nForm #" << (i + 1) << std::endl;
+		outputAllFormInfo(*List.FormList.at(i));
+		std::cout << std::endl;
+	}
+}
+
+void removeFormList(ShippingFormList &List) {
+	char choice = 'N';
+	std::cout << "\nThe current database has " << List.FormList.size() << " forms\n";
+	std::cout << "Do you want to remove a form? (Y/N) :";
+	std::cin >> choice;
+
+	if (choice == 'Y' || choice == 'y')
+		do {
+			int number_of_forms = List.FormList.size();
+			if (number_of_forms <= 0) {
+				std::cout << "No forms available to remove\n";
+				return;
+			}
+			else {
+				int form_index = 0;
+				do {
+					std::cout << "Choose a form index to remove [0 - " << number_of_forms - 1 << "]: ";
+					std::cin >> form_index;
+				} while (form_index < 0 || form_index >= number_of_forms);
+
+				List.removeForm(form_index);
+				std::cout << "Form number [" << form_index << "] has been successfully removed\n";
+
+				std::cout << "\nThe current database has " << number_of_forms - 1 << " forms\n";
+				std::cout << "Do you want to remove more forms? (Y/N) :";
+				std::cin >> choice;
+			}
+		} while (choice == 'Y' || choice == 'y');
+}
+
+void editFormList(ShippingFormList &List) {
+	char choice = 'N';
+	std::cout << "The current database has " << List.FormList.size() << " forms\n";
+	std::cout << "Do you want to edit more forms? (Y/N) :";
+	std::cin >> choice;
+
+	if (choice == 'Y' || choice == 'y')
+		do {
+			int number_of_forms = List.FormList.size();
+			if (number_of_forms <= 0) {
+				std::cout << "No forms available to edit\n";
+				return;
+			}
+			else {
+				int form_index = 0;
+				do {
+					std::cout << "Choose a form index to edit [0 - " << number_of_forms - 1 << "]: ";
+					std::cin >> form_index;
+				} while (form_index < 0 || form_index >= number_of_forms);
+
+				
+				std::cout << "Editing form number [" << form_index << "]:\n\n";
+				ShippingForm* NewForm;
+				inputForm(NewForm);
+				List.replaceForm(NewForm, form_index);
+
+				std::cout << "\nThe current database has " << number_of_forms << " forms\n";
+				std::cout << "Do you want to edit more forms? (Y/N) :";
+				std::cin >> choice;
+			}
+		} while (choice == 'Y' || choice == 'y');
 }
