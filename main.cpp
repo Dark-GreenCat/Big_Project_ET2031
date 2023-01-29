964da5a490e7840b46bc4bd88a1f58d2f7e639e4
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2023-01-29 17:00:22 +0700

    Merge manually main.cpp from experimental-huy

--- main.cpp
+++ main.cpp
@@ -1,21 +1,62 @@
 #include "data_structure.h"
+#include "function.h"
 #include <iostream>
 #include <string>
 #include <cstring>
 #include <vector>
-#include <fstream>
+
 using namespace std;
 
 #define MAX_NUMBER_OF_FORM 100
 
-int main()
-{
+int main() { 
+    ShippingFormList List;
+    
     ifstream filein;
     filein.open("infor.text", ios_base::in);
-    ShippingFormList pLIST;
-    pLIST.inputList(filein);
-    pLIST.outputList();
+    List.inputList(filein);
+    printFormList(List);
     filein.close();
 
-    return 0;
+    int option;
+    /* DELIVERY FORMS MANAGERMENT PROGRAM */
+    while (true) {
+        cout << endl;
+        printMenu();
+        cin >> option;
+
+        // User input wrong data
+        if (!cin) {
+            // user didn't input a number
+            cin.clear();                      // reset failbit
+            cin.ignore(MAX_STREAMSIZE, '\n'); // skip bad input
+        }
+
+        switch (option) {
+            case 1: // Add a form
+                inputFormList(List);
+                break;
+
+            case 2: // Edit a form
+                editFormList(List);
+                break;
+
+            case 3: // Remove a form
+                removeFormList(List);
+                break;
+
+            case 8: // Print all forms
+                printFormList(List);
+                break;
+
+            case 9:
+                List.FormList.clear();
+                exit(1);
+                break;
+
+            default:
+                cout << "This feature is currently not available\n";   
+                break;
+        }
+    }
 }
